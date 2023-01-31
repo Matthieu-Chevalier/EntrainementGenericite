51ef3998dd6f1d4e7c74f23dd2931f292a160d1d
Matthieu <matthieu.chevalier.pro@gmail.com>
2023-01-31 15:12:44 +0100

    Repos OK

---
+++ EntityModel/Repositories/GenericReadOnlyRepository.cs
@@ -0,0 +1,39 @@
+﻿using EntityModel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityModel.Repositories
+{
+    public class GenericReadOnlyRepository<TEntity> : IGenericReadOnlyRepository<TEntity> where TEntity : AbstractEntity
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericReadOnlyRepository(DbContext dbContext)
+        {
+            //TODO : gérer la possibilité d'un context null
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+
+            //Désactive le trackage des entités (car lecture seule)
+            _dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public IList<TEntity> GetAll() => _dbSet.AsNoTracking().ToList();
+        public TEntity GetById(int id) => _dbSet.Find(id);
+
+        public IList<TEntity> FindAllWhere(Expression<Func<TEntity, bool>> expression)
+        {
+            return _dbSet.Where(expression).ToList();
+        }
+
+        
+
+       
+    }
+}
---
+++ EntityModel/Repositories/IGenericReadOnlyRepository.cs
@@ -0,0 +1,35 @@
+﻿using EntityModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityModel.Repositories
+{
+    /// <summary>
+    /// Cette interface générique défini les méthodes de lecture uniquement.
+    /// </summary>
+    /// <typeparam name="TEntity">L'entité à manipuler</typeparam>
+    public interface IGenericReadOnlyRepository<TEntity> where TEntity : AbstractEntity
+    {
+        /// <summary>
+        /// Retourne une entité si elle existe
+        /// </summary>
+        /// <param name="id">identifiant de l'entité</param>
+        /// <returns>L'entité trouvée, ou null</returns>
+        public TEntity GetById(int id);
+        /// <summary>
+        /// Retourne la totalité des entitées
+        /// </summary>
+        /// <returns></returns>
+        public IList<TEntity> GetAll();
+        /// <summary>
+        /// Retourne la toatalité des entités respectant une expression
+        /// </summary>
+        /// <param name="expression">Exepression lambda</param>
+        /// <returns>Liste d'entités</returns>
+        public IList<TEntity> FindAllWhere(Expression<Func<TEntity, bool>> expression);
+    }
+}
